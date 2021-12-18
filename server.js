34e46fa0d965f0d39ab8ffebba1e9a28863ce880
Catanna82 <anna820614@gmail.com>
2021-12-18 12:28:04 +0200

    Update access CORS

--- server.js
+++ server.js
@@ -35,8 +35,11 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
+    res.header("Access-Control-Allow-Origin", "https://janphotosite.web.app/"); // update to match the domain you will make the request from
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");//
     next();
 });
+
 
 const Schema = mongo.Schema;
 
