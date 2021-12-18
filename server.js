24af98a185a8633cf984a5585bb1acc9b45195ba
Catanna82 <anna820614@gmail.com>
2021-12-18 12:33:19 +0200

    update

--- server.js
+++ server.js
@@ -35,8 +35,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
-    res.setHeader("Access-Control-Allow-Origin", "https://janphotosite.web.app/"); // update to match the domain you will make the request from
-    res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");//
     next();
 });
 
